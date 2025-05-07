0bcbdb0c27c91804cd847e5c4095635568e1fc19
Agahnim <gabrielguysoulard@gmail.com>
2025-05-07 15:31:41 +0200

    wip home

--- src/pages/HomeContent.tsx
+++ src/pages/HomeContent.tsx
@@ -1,18 +1,28 @@
+import { Marquee } from "../components/Marquee";
+
 export default function HomeContent() {
     return (
         <div className="bg-light-pink-ouga bg-cover bg-center h-screen w-screen overflow-hidden">
-            <div className="bg-radial-[at_center] from-pink-ouga from-40% to-lavender-ouga to-90% relative z-10 flex flex-col items-center px-4 space-y-4 max-w-4xl mx-auto p-6 mt-4">
+            <div className="relative z-10 flex flex-col items-center px-4 space-y-4 max-w-4xl mx-auto p-6 mt-4">
+                <div className="w-[400px]">
+                    <img src="/images/welcome.webp" width="400px"/>
+                    <Marquee className="py-1 mb-4 font-dotgothic">&nbsp;彡★Welcome to my little corner of the Web★彡&nbsp;</Marquee>
+                </div>
                 <div className="flex space-x-4">
                     <div className="bg-white bg-opacity-80 p-4 rounded shadow-md flex-1">
                         <p className="text-sm font-dotgothic">
-                            Hey ! I'm Agahnim, a French developer and music producer.
+                            Thank you for visiting my website !
+                            For now it's still a <span className="text-darkerer-pink-ouga">work in progress</span> but I'm sure you will see changes if you come back from time to time :)
+                            <br/><br/>
+                            My name is <span className="text-darkerer-pink-ouga">Agahnim</span> 
+                            <span className="ml-1 px-2 py-[1px] text-xs rounded-full bg-blue-ouga text-white font-semibold">she/her</span>, I'm a <span className="text-darkerer-pink-ouga">developer</span> and <span className="text-darkerer-pink-ouga">music producer</span> from <span className="text-darkerer-pink-ouga">France</span> !
+                            <br/><br/>
+                            I created this website not just to <span className="text-darkerer-pink-ouga">improve</span> my web development skills, but also because it's <span className="inline-block text-darkerer-pink-ouga animate-bounce">fun</span> to build a project like this. It's a small, personal space on the internet, away from the social climate, which is getting <span className="text-red-700">worse and worse</span>
                         </p>
                     </div>
 
-                    <div className="bg-white bg-opacity-80 p-4 rounded shadow-md flex-1">
-                        <p className="text-sm font-dotgothic">
-                            Et quand l’ombre viendra, que la musique demeure, veilleuse éternelle de mes pensées.
-                        </p>
+                    <div className="bg-white bg-opacity-80 p-4 rounded shadow-md flex-[0.3]">
+
                     </div>
                 </div>
             </div>
