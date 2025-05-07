76bd08bf432a1ee6175fd683d8b4689fc274f031
Agahnim <gabrielguysoulard@gmail.com>
2025-05-07 11:30:39 +0200

    update bg

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,7 @@ export default function About() {
         <div className="bg-light-pink-ouga bg-cover bg-center h-screen w-screen">
             <Navbar />
             <video className="fixed top-0 left-0 w-screen h-screen object-cover z-0" autoPlay muted loop src="/videos/about.mp4" preload="auto"></video>
-            <div className="bg-radial from-pink-ouga to-lavender-ouga relative z-10 flex flex-col items-center px-4 space-y-4 max-w-4xl mx-auto p-6 rounded mt-4">
+            <div className="bg-radial-[at_center] from-pink-ouga from-40% to-lavender-ouga to-90% relative z-10 flex flex-col items-center px-4 space-y-4 max-w-4xl mx-auto p-6 rounded mt-4">
                 <div className="flex space-x-4">
                     <div className="bg-white bg-opacity-80 p-4 rounded shadow-md flex-1">
                         <p className="text-sm font-dotgothic">
--- src/pages/Dev.tsx
+++ src/pages/Dev.tsx
@@ -5,7 +5,7 @@ export default function Dev() {
         <div className="bg-light-pink-ouga bg-cover bg-center h-screen w-screen">
             <Navbar />
             <video className="fixed top-0 left-0 w-screen h-screen object-cover z-0" autoPlay muted loop src="/videos/dev.mp4" preload="auto"></video>
-            <div className="bg-radial from-pink-ouga to-lavender-ouga mt-4 relative z-10 flex flex-col items-center px-4 space-y-4 max-w-4xl mx-auto p-6">
+            <div className="bg-radial-[at_center] from-pink-ouga from-40% to-lavender-ouga to-90% mt-4 relative z-10 flex flex-col items-center px-4 space-y-4 max-w-4xl mx-auto p-6">
                 <div className="bg-white bg-opacity-80 p-4 rounded shadow-md">
                     <p className="text-sm font-dotgothic">
                         I always had an interest in development. 
