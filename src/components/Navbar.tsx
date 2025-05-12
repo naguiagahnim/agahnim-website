954e52473612b42c5fa5a61d01dbbdd632498be4
Agahnim <gabrielguysoulard@gmail.com>
2025-05-12 16:10:05 +0200

    👵

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Home, User, Heart, Terminal, Music, NotebookPen } from 'lucide-react';
+import { Home, User, Heart, Terminal, Music } from 'lucide-react';
 import '../styles/navbar.css';
 
 export default function Navbar() {
